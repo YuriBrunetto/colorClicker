b2535ce5c976dbc5a30b8976bc2bcecc7c0b9f22
Yuri André Brunetto <yuribrunetto@hotmail.com.br>
2015-06-01 01:33:37 -0300

    Layout changes

--- core.js
+++ core.js
@@ -211,7 +211,7 @@
                 for (var i in Game.Objects) {
                     var me = Game.Objects[i];
 
-                    str += "<div class='store-a' onclick='Game.ObjectsById[" + me.id + "].buy();' id='" + me.id + "' title='" + me.name + "'>" + me.name + " <span class='store-span'>Cost: " + Beautify(me.price) + " fragments</span></div>";
+                    str += "<div class='store-a' onclick='Game.ObjectsById[" + me.id + "].buy();' id='" + me.id + "' title='" + me.name + "'>" + me.name + " <span class='store-span'>Cost: " + Beautify(me.price) + " fragments</span><div class='clear-both'></div><div class='store-desc'>" + me.desc + "</div></div>";
                 }
 
                 l('products').innerHTML = str;
@@ -330,9 +330,10 @@
                 return this;
             }
 
-            new Game.Object("Pencil", "Gives +0.1 fragments per second.", 15, 2, 0.1, 0, 0, 1);
-            new Game.Object("Ink", "Gives +0.2 fragments per second.", 30, 3, 0.2, 0, 0, 2);
-            new Game.Object("Paint Brush", "Gives +0.3 fragments per second.", 50, 4, 0.3, 0, 1, 1);
+            // name, desc, price, increase, cps, red, green, blue
+            new Game.Object("Pencil", "Gives to you 1 point of blue and 0.1 fragments per second.", 50, 2, 0.1, 0, 0, 1);
+            new Game.Object("Ink", "Gives to you 2 points of blue and 0.1 fragments per second.", 150, 3, 0.1, 0, 0, 2);
+            new Game.Object("Paint Brush", "Gives to you 1 point of green and 0.2 fragments per second.", 450, 4, 0.2, 0, 1, 0);
 
             Game.ComputeCps = function(base, add, mult, bonus) {
                 if (!bonus) bonus = 0;
